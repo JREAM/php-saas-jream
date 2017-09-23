7e00a93226a69c2f8b73b22060615a63e9d8a8ff
Jesse Boyer <imboyus@gmail.com>
2017-09-22 20:34:14 -0400

    upd

--- app/controllers/api/AuthController.php
+++ app/controllers/api/AuthController.php
@@ -114,8 +114,6 @@ class AuthController extends ApiController
                 $plus = new \Google_Service_Plus_Person($client);
                 // @TODO Save to DB if not exists, otherwise login, refresh token?
                 echo '<pre>';
-                $a=new \Google_Service_Plus_Person($client);
-                $a->
                 print_r($plus);
                 print_r($plus->getEmails());
                 print_r($plus->getDisplayName());

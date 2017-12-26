334c58fe9dccd0049dd11ac7c5638dee21cbe271
Jesse Boyer <imboyus@gmail.com>
2017-12-26 09:11:19 -0500

    cleanin

---
+++ resources/assets/js/vue/Question.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  <textarea></textarea>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data: () => return {
+
+  }
+}
+</script>
+
+<style scoped>
+textarea {
+  border: 5px solid red;
+}
+</style>

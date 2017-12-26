0be47c59dfd72efa8fb554262048dd0e8e44a57c
Jesse Boyer <imboyus@gmail.com>
2017-12-26 09:49:17 -0500

    local installs setup tools locally also

--- resources/setup-local.sh
+++ resources/setup-local.sh
@@ -15,3 +15,11 @@ if [[ ! -f .env ]]; then
   cp .env.sample .env
   echo 'Edit the .env file'
 fi
+
+echo "Setting Up Phalcon Devtools"
+cd ~
+git clone https://github.com/phalcon/phalcon-devtools.git
+cd phalcon-devtools
+sudo ln -s $(pwd)/phalcon.php /usr/bin/phalcon
+sudo chmod ugo+x /usr/bin/phalcon
+echo "Done, you should be able to run $ phalcon"
